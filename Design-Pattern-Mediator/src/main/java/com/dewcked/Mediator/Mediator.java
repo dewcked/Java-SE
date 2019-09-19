8e4f18d6c52397e728390ef432665651742c10e9
dewcked <rkdcksdn1234@gmail.com>
2019-09-19 21:07:53 +0900

    refactored v1

--- Design-Pattern-Mediator/src/main/java/com/dewcked/Mediator/Mediator.java
+++ Design-Pattern-Mediator/src/main/java/com/dewcked/Mediator/Mediator.java
@@ -2,12 +2,14 @@ package com.dewcked.Mediator;
 
 public class Mediator {
 	Welcome welcome;
+	Login login;
 	Shop shop;
 	Purchase purchase;
 	Exit exit;
 
 	public Mediator() {
 		welcome = new Welcome(this);
+		login = new Login(this);
 		shop = new Shop(this);
 		purchase = new Purchase(this);
 		exit = new Exit(this);
@@ -16,8 +18,12 @@ public class Mediator {
 	public void handle(String state) {
 		if (state.equals("welcome.shop")) {
 			shop.go();
+		} else if (state.equals("welcome.login")) {
+			login.go();
 		} else if (state.equals("shop.purchase")) {
 			purchase.go();
+		} else if (state.equals("login.exit")) {
+			exit.go();
 		} else if (state.equals("purchase.exit")) {
 			exit.go();
 		} else if (state.equals("welcome.exit")) {
