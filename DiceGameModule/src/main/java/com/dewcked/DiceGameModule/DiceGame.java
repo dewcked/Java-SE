3d0d589e01fae16f9acc02faf223b986ccc281d6
dewcked <rkdcksdn1234@gmail.com>
2019-09-19 18:28:03 +0900

    refactored program

--- DiceGameModule/src/main/java/com/dewcked/DiceGameModule/DiceGame.java
+++ DiceGameModule/src/main/java/com/dewcked/DiceGameModule/DiceGame.java
@@ -1,49 +1,44 @@
 package com.dewcked.DiceGameModule;
+
 /**
- * @DiceGame.java
- * @author Joseph Gihwon Kwon -- Here comes that dreamer !
- * @date 2016-10-02
+ * @author  Joseph Gihwon Kwon -- Here comes that dreamer !
+ * @date 	2016-10-02
  */
+
 public class DiceGame {
-	//Change this value to change number of dice
-	private static int dienum = 3;
-	private Die[] dice;
-
-	public int GetDiceNum(){
-		return dienum;
+	private Die die1;
+	private Die die2;
+	private Die die3;
+	private Player gamer;
+	public enum Result {win, tie, lose};
+	
+	public DiceGame(){
+		die1 = new Die();
+		die2 = new Die();
+		die3 = new Die();
+		gamer = new Player("Kim Game");
 	}
-	public DiceGame() {
-		dice = new Die[dienum];
-		for(int i=0;i<dienum;i++) {
-			dice[i] = new Die();
-		}
-	}
 
-	public void play() {
-		int[] fv = new int[dienum];
-		int sum = 0;
-		while (true) {
-			for(int i=0;i<dienum;i++) {
-				dice[i].roll();
-				fv[i] = dice[i].getFaceValue();
-				sum += fv[i];
-			}
-			if (sum > 14) {
-				System.out.print("합이 " + sum + "(");
-				for(int i=0;i<dienum-1;i++)
-					System.out.print(fv[i] + ",");
-				System.out.println(fv[dienum-1] + ") 이어서 승리");
+	public static void main(String[] args) {
+		DiceGame dicegame = new DiceGame();
+		dicegame.initialize();
+	}
+	
+	public void initialize() {
+		Result result = null;
+		
+		while(true){
+			result = gamer.play(die1, die2, die3);
+			
+			if(result == Result.win) {
+				System.out.println("�빀�씠 15 �씠�긽 �씠�뼱�꽌 �듅由�");
 				break;
-			} else if(sum < 15 && sum>9){
-				System.out.print("합이 " + sum + "(");
-				for(int i=0;i<dienum-1;i++)
-					System.out.print(fv[i] + ",");
-				System.out.println(fv[dienum-1] + ") 이어서 비김");
-			} else {
-				System.out.print("합이 " + sum + "(");
-				for(int i=0;i<dienum-1;i++)
-					System.out.print(fv[i] + ",");
-				System.out.println(fv[dienum-1] + ") 이어서 패배");
+			}
+			else if(result == Result.tie){
+				System.out.println("�빀�씠 10~14 �씠�뼱�꽌 鍮꾧�");
+			}
+			else if(result == Result.lose) {
+				System.out.println("�빀�씠 10 �씠�븯(誘몃쭔)�뿬�꽌  �뙣諛�");
 			}
 		}
 	}
--- DiceGameModule/src/main/java/com/dewcked/DiceGameModule/Player.java
+++ DiceGameModule/src/main/java/com/dewcked/DiceGameModule/Player.java
@@ -1,13 +1,35 @@
 package com.dewcked.DiceGameModule;
+
+import com.dewcked.DiceGameModule.DiceGame.Result;
+
 /**
- * @Player.java
- * @author Joseph Gihwon Kwon -- Here comes that dreamer !
- * @date 2016-10-02
+ * @author  Joseph Gihwon Kwon -- Here comes that dreamer !
+ * @date 	2016-10-02
  */
+ 
 public class Player {
-// private String name;
-	public static void main(String[] args) {
-		DiceGame dicegame = new DiceGame();
-		dicegame.play();
+	private String name;
+	/* 2019-09-19 KCW */
+	/* manually added */
+	/* without this code, program would not run! */
+	public Player(String string) {
+		this.name = string;
 	}
+	/* 2019-09-19 KCW */
+	/* manually added */
+	/* without this code, program would not run! */
+	public Result play(Die die1, Die die2, Die die3) {
+		Result result = null;
+		die1.roll();
+		die2.roll();
+		die3.roll();
+		int sum = die1.getFaceValue() + die2.getFaceValue() + die3.getFaceValue();
+		if(sum >= 15)
+			result = Result.win;
+		else if(sum >= 10 && sum <= 14)
+			result = Result.tie;
+		else
+			result = Result.lose;
+		return result;
+	};
 }
