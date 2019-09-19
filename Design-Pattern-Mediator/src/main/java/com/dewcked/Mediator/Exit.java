7da3ea8989167e1e6be07678959a025949afb9ed
dewcked <rkdcksdn1234@gmail.com>
2019-09-19 19:05:51 +0900

    removed unreachable code

--- Design-Pattern-Mediator/src/main/java/com/dewcked/Mediator/Exit.java
+++ Design-Pattern-Mediator/src/main/java/com/dewcked/Mediator/Exit.java
@@ -1,16 +1,13 @@
 package com.dewcked.Mediator;
 
-public class Exit
-{
-  Mediator mediator;
+public class Exit {
+	Mediator mediator;
 
-  public Exit(Mediator m)
-  {
-    mediator = m;
-  }
+	public Exit(Mediator m) {
+		mediator = m;
+	}
 
-  public void go()
-  {
-    System.out.println("안녕히 가세요~.");
-  }
+	public void go() {
+		System.out.println("안녕히 가세요~.");
+	}
 }
--- Design-Pattern-Mediator/src/main/java/com/dewcked/Mediator/Shop.java
+++ Design-Pattern-Mediator/src/main/java/com/dewcked/Mediator/Shop.java
@@ -2,33 +2,28 @@ package com.dewcked.Mediator;
 
 import java.io.*;
 
-public class Shop
-{
-  Mediator mediator;
-  String response = "n";
+public class Shop {
+	Mediator mediator;
+	String response = "n";
 
-  public Shop(Mediator m)
-  {
-    mediator = m;
-  }
+	public Shop(Mediator m) {
+		mediator = m;
+	}
 
-  public void go()
-  {
-    System.out.print(
-      "구매하시겠어요? [y/n]? ");
-    BufferedReader reader = new 
-      BufferedReader(new InputStreamReader(System.in));
+	public void go() {
+		System.out.print("구매하시겠어요? [y/n]? ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    try{
-      response = reader.readLine();
-    } catch (IOException e){ 
-      System.err.println("Error");
-    }
+		try {
+			response = reader.readLine();
+		} catch (IOException e) {
+			System.err.println("Error");
+		}
 
-    if (response.equals("y")){
-      mediator.handle("shop.purchase");
-    } else {
-      mediator.handle("shop.exit");
-    }
-  }
+		if (response.equals("y")) {
+			mediator.handle("shop.purchase");
+		} else {
+			mediator.handle("shop.exit");
+		}
+	}
 }
