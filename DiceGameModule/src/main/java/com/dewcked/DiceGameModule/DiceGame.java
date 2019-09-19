ba9adb9c1301b6d9c35a2f99485cc346ea1bfbea
KGU-PC <KGU-PC@20.38.101.29>
2019-09-19 18:53:36 +0900

    bad example

--- DiceGameModule/src/main/java/com/dewcked/DiceGameModule/DiceGame.java
+++ DiceGameModule/src/main/java/com/dewcked/DiceGameModule/DiceGame.java
@@ -1,21 +1,13 @@
 package com.dewcked.DiceGameModule;
 
-/**
- * @author  Joseph Gihwon Kwon -- Here comes that dreamer !
- * @date 	2016-10-02
- */
-
 public class DiceGame {
 	private Die die1;
 	private Die die2;
-	private Die die3;
 	private Player gamer;
-	public enum Result {win, tie, lose};
 	
 	public DiceGame(){
 		die1 = new Die();
 		die2 = new Die();
-		die3 = new Die();
 		gamer = new Player("Kim Game");
 	}
 
@@ -25,20 +17,17 @@ public class DiceGame {
 	}
 	
 	public void initialize() {
-		Result result = null;
+		boolean win;
 		
 		while(true){
-			result = gamer.play(die1, die2, die3);
+			win = gamer.play(die1, die2);
 			
-			if(result == Result.win) {
-				System.out.println("�빀�씠 15 �씠�긽 �씠�뼱�꽌 �듅由�");
+			if(win) {
+				System.out.println("합이 7 이어서 승리");
 				break;
 			}
-			else if(result == Result.tie){
-				System.out.println("�빀�씠 10~14 �씠�뼱�꽌 鍮꾧�");
-			}
-			else if(result == Result.lose) {
-				System.out.println("�빀�씠 10 �씠�븯(誘몃쭔)�뿬�꽌  �뙣諛�");
+			else{
+				System.out.println("합이 7이 아니어서 패배");
 			}
 		}
 	}
