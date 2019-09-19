5b81cef77ea728305b030d0c7ad1cdcadbb986d5
dewcked <rkdcksdn1234@gmail.com>
2019-09-19 19:02:09 +0900

    removed unreachable code

--- Design-Pattern-Mediator/src/main/java/com/dewcked/Mediator/TestMediator.java
+++ Design-Pattern-Mediator/src/main/java/com/dewcked/Mediator/TestMediator.java
@@ -1,16 +1,13 @@
 package com.dewcked.Mediator;
 
-public class TestMediator
-{
-  public static void main(String args[])
-  {
-    TestMediator t = new TestMediator();
-  }
+public class TestMediator {
+	public static void main(String args[]) {
+		TestMediator t = new TestMediator();
+	}
 
-  public TestMediator()
-  {
-    Mediator mediator = new Mediator();
+	public TestMediator() {
+		Mediator mediator = new Mediator();
 
-    mediator.getWelcome().go();
-  }
+		mediator.getWelcome().go();
+	}
 }
